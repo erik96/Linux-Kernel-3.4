f47214c556f426b40d18dd48e75c506aa1a0b715
erik96 <ariesrik96@gmail.com>
2013-04-27 10:47:24 +0300

    Linux 3.4.5
    patch -p1 < ../patch-3.4.4-5

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1062,7 +1062,7 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 	dpm_wait_for_children(dev, async);
 
 	if (async_error)
-		return 0;
+		goto Complete;
 
 	pm_runtime_get_noresume(dev);
 	if (pm_runtime_barrier(dev) && device_may_wakeup(dev))
@@ -1071,7 +1071,7 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 	if (pm_wakeup_pending()) {
 		pm_runtime_put_sync(dev);
 		async_error = -EBUSY;
-		return 0;
+		goto Complete;
 	}
 
 	data.dev = dev;
